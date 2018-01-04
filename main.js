c4e24c9c55efbf5b50fb77a5e3617a523be75c73
SEO\TIvasyuk <tetyana.ivasyuk@onseo.biz>
2018-01-04 13:21:05 +0200

    add favicon and some changes in ts

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
 var SlotMachine = (function () {
     function SlotMachine() {
-        this.test = new Array();
+        this.coord = [];
+        this.img = [];
+        this.game = document.getElementById("game");
+        this.ctx = this.game.getContext("2d");
+        this.pic = new Image();
+        this.pic.src = "img/gems-s3ec6b0050c.png";
+        for (var i = 0; i <= 6; i++) {
+            this.coord[i] = i * this.loadConfig().size;
+        }
+        for (var i = 0; i < 90; i++) {
+            this.img.push(this.coord[Math.floor(Math.random() * this.coord.length)]);
+        }
         this.loadImages();
+        this.animate();
     }
     SlotMachine.prototype.loadImages = function () {
         var data = this.loadConfig();
-        var game = document.getElementById("game");
-        var ctx = game.getContext("2d");
-        var lines = game.getContext("2d");
+        var game = this.game;
+        var ctx = this.ctx;
+        var pic = this.pic;
         game.width = data.columns * data.sizeInField + (data.columns + 1) * data.marginX;
         game.height = data.rows * data.sizeInField + (data.rows + 1) * data.marginY;
-        /*var grd = lines.createLinearGradient(0, 0, 0, game.height);
+        var img = this.img;
+        var coord = this.coord;
+        //draw lines in field
+        var grd = ctx.createLinearGradient(0, 0, 0, game.height);
         grd.addColorStop(0, "transparent");
         grd.addColorStop(0.5, "#eba8d3");
         grd.addColorStop(1, "transparent");
-        lines.fillStyle = grd;
-
-        lines.shadowOffsetX = 0;
-        lines.shadowOffsetY = 0;
-        lines.shadowBlur = 20;
-        lines.shadowColor = '#eba8d3';
-
+        ctx.fillStyle = grd;
         for (var i = 0; i < 7; i++) {
-            lines.fillRect(20 + data.sizeInField * i + data.marginX * i, 0, 4, game.height);
-        }*/
-        var pic = new Image();
-        pic.src = "img/gems-s3ec6b0050c.png";
-        var img = [];
-        var coord = [];
-        for (var i = 0; i <= 6; i++) {
-            coord[i] = i * data.size;
+            ctx.fillRect(20 + data.sizeInField * i + data.marginX * i, 0, 4, game.height);
         }
-        for (var i = 0; i < 90; i++) {
-            img.push(coord[Math.floor(Math.random() * coord.length)]);
-        }
+        //first draw image in game layer
         pic.onload = function () {
             var k = 0;
             for (var i = 0; i < 5; i++) {
+                var dataPosX = data.marginX * (i + 1) + i * data.sizeInField;
                 for (var j = 0; j < 3; j++) {
-                    var dataPosX = data.marginX * (i + 1) + i * data.sizeInField;
                     var dataPosY = data.marginY * (j + 1) + j * data.sizeInField;
                     //sprite position in x, in y. size in x, in y. field position in x, in y. width, height.
                     ctx.drawImage(pic, 0, img[k], data.size, data.size, dataPosX, dataPosY, data.sizeInField, data.sizeInField);
@@ -46,22 +46,25 @@ var SlotMachine = (function () {
                 }
             }
         };
-        var offsetY = 0;
+    };
+    SlotMachine.prototype.animate = function () {
+        var data = this.loadConfig();
+        var game = this.game;
+        var ctx = this.ctx;
+        var pic = this.pic;
+        var coord = this.coord;
+        var img = this.img;
+        //draw animation
         var array = new Array();
         function draw(col) {
             var k = col * 18;
             var dataPosX = data.marginX * (col + 1) + col * data.sizeInField;
             ctx.clearRect(dataPosX, 0, data.sizeInField, game.height);
-            for (var j = 90; j >= 0; j--) {
+            for (var j = 89; j >= 0; j--) {
                 var dataPosY = -array[col].offsetY + j * (data.sizeInField + data.marginY) + data.marginY;
                 ctx.drawImage(pic, 0, img[k], data.size, data.size, dataPosX, dataPosY, data.sizeInField, data.sizeInField);
-                if (k == 90) {
-                    k = 0;
-                }
-                else {
-                    k++;
-                }
-                if (array[col].offsetY < data.sizeInField * 91) {
+                k == 90 ? k = 0 : k++;
+                if (array[col].offsetY < 87 * data.sizeInField + 87 * data.marginY) {
                     array[col].offsetY++;
                 }
                 else {
@@ -71,29 +74,6 @@ var SlotMachine = (function () {
                 }
             }
         }
-        $(function () {
-            $("#spin").click(function () {
-                $("#spin").attr('disabled', "disabled");
-                for (var i = 0; i < array.length; i++) {
-                    array[i].offsetY = 0;
-                    clearInterval(array[i].interval);
-                }
-                for (var i = 0; i < 5; i++) {
-                    var callback;
-                    if (i == 4) {
-                        callback = function () {
-                            $("#spin").removeAttr('disabled');
-                        };
-                    }
-                    createColumn(i, data.speed * i, callback);
-                }
-                array = [];
-                img = [];
-                for (var i = 0; i < 90; i++) {
-                    img.push(coord[Math.floor(Math.random() * coord.length)]);
-                }
-            });
-        });
         function createColumn(col, timeout, callback) {
             setTimeout(function () {
                 array.push({
@@ -103,6 +83,29 @@ var SlotMachine = (function () {
                 });
             }, timeout);
         }
+        $(function () {
+            $("#spin").on("click", function () {
+                $("#spin").attr('disabled', "disabled");
+                for (var i = 0; i < array.length; i++) {
+                    array[i].offsetY = 0;
+                    clearInterval(array[i].interval);
+                }
+                for (var i = 0; i < 5; i++) {
+                    var callback = void 0;
+                    if (i == 4) {
+                        callback = function () {
+                            $("#spin").removeAttr('disabled');
+                        };
+                    }
+                    createColumn(i, data.speed * i, callback);
+                }
+                array.length = 0;
+                img.length = 0;
+                for (var i = 0; i < 90; i++) {
+                    img.push(coord[Math.floor(Math.random() * coord.length)]);
+                }
+            });
+        });
     };
     SlotMachine.prototype.loadConfig = function () {
         var data;
--- main.ts
+++ main.ts
@@ -1,121 +1,120 @@
 ﻿class SlotMachine {
-
-    private test = new Array<HTMLImageElement>();
-
+    private game: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D;
+    private pic: HTMLImageElement;
+    private coord: Array<number> = [];
+    private img: Array<number> = [];
 
     constructor() {
+        this.game = <HTMLCanvasElement>document.getElementById("game");
+        this.ctx = this.game.getContext("2d");
+        this.pic = new Image();
+        this.pic.src = "img/gems-s3ec6b0050c.png";
+
+        for (let i = 0; i <= 6; i++) {
+            this.coord[i] = i * this.loadConfig().size;
+        }
+        for (let i = 0; i < 90; i++) {
+            this.img.push(this.coord[Math.floor(Math.random() * this.coord.length)]);
+        }
+
         this.loadImages();
+        this.animate();
     }
 
     private loadImages(): void {
-
-        var data = this.loadConfig();
-
-        let game = (<HTMLCanvasElement>document.getElementById("game"));
-        let ctx = game.getContext("2d");
-        let lines = game.getContext("2d");
-
+        let data = this.loadConfig();
+        let game = this.game;
+        let ctx = this.ctx;
+        let pic = this.pic;
         game.width = data.columns * data.sizeInField + (data.columns + 1) * data.marginX;
         game.height = data.rows * data.sizeInField + (data.rows + 1) * data.marginY;
-
-
-        /*var grd = lines.createLinearGradient(0, 0, 0, game.height);
+        let img = this.img;
+        let coord = this.coord;
+        
+        //draw lines in field
+        let grd = ctx.createLinearGradient(0, 0, 0, game.height);
         grd.addColorStop(0, "transparent");
         grd.addColorStop(0.5, "#eba8d3");
         grd.addColorStop(1, "transparent");
-        lines.fillStyle = grd;
-
-        lines.shadowOffsetX = 0;
-        lines.shadowOffsetY = 0;
-        lines.shadowBlur = 20;
-        lines.shadowColor = '#eba8d3';
-
-        for (var i = 0; i < 7; i++) {
-            lines.fillRect(20 + data.sizeInField * i + data.marginX * i, 0, 4, game.height);
-        }*/
-
-
-        let pic = new Image();
-        pic.src = "img/gems-s3ec6b0050c.png";
-
-        let img: Array<number> = [];
-        let coord: Array<number> = [];
-
-        for (var i = 0; i <= 6; i++) {
-            coord[i] = i * data.size;
+        ctx.fillStyle = grd;
+        for (let i = 0; i < 7; i++) {
+            ctx.fillRect(20 + data.sizeInField * i + data.marginX * i, 0, 4, game.height);
         }
 
-        for (var i = 0; i < 90; i++) {
-            img.push(coord[Math.floor(Math.random() * coord.length)]);
-        }
-
-
+        //first draw image in game layer
         pic.onload = () => {
-            var k = 0;
-            for (var i = 0; i < 5; i++) {
-                for (var j = 0; j < 3; j++) {
-                    var dataPosX = data.marginX * (i + 1) + i * data.sizeInField;
-                    var dataPosY = data.marginY * (j + 1) + j * data.sizeInField;
-
+            let k: number = 0;
+            for (let i = 0; i < 5; i++) {
+                let dataPosX: number = data.marginX * (i + 1) + i * data.sizeInField;
+                for (let j = 0; j < 3; j++) {
+                    let dataPosY: number = data.marginY * (j + 1) + j * data.sizeInField;
                     //sprite position in x, in y. size in x, in y. field position in x, in y. width, height.
                     ctx.drawImage(pic, 0, img[k], data.size, data.size, dataPosX, dataPosY, data.sizeInField, data.sizeInField);
                     k++;
                 }
             }
         }
+    }
+    private animate(): void {
+        let data = this.loadConfig();
+        let game = this.game;
+        let ctx = this.ctx;
+        let pic = this.pic;
+        let coord = this.coord;
+        let img = this.img;
 
+        //draw animation
+        let array = new Array<Object>();
 
-        var offsetY = 0;
-
-        var array = new Array<Object>();
-
-        function draw(col) {
-
-            var k = col * 18;
-            var dataPosX = data.marginX * (col + 1) + col * data.sizeInField;
+        function draw(col:number) {
+            let k:number = col * 18;
+            let dataPosX:number = data.marginX * (col + 1) + col * data.sizeInField;
 
             ctx.clearRect(dataPosX, 0, data.sizeInField, game.height);
 
-            for (var j = 90; j >= 0; j--) {
-                var dataPosY = -array[col].offsetY + j * (data.sizeInField + data.marginY) + data.marginY;
+            for (let j = 89; j >= 0; j--) {              
+                let dataPosY:number = -array[col].offsetY + j * (data.sizeInField + data.marginY) + data.marginY;
 
                 ctx.drawImage(pic, 0, img[k], data.size, data.size, dataPosX, dataPosY, data.sizeInField, data.sizeInField);
 
-                if (k == 90) {
-                    k = 0;
-                } else {
-                    k++;
-                }
+                k == 90 ? k = 0 : k++;
 
-                if (array[col].offsetY < data.sizeInField * 91) {
+                if (array[col].offsetY < 87 * data.sizeInField + 87 * data.marginY) {
                     array[col].offsetY++;
                 }
-                else
-                {
+                else {
                     if (array[col].callback != null) {
                         array[col].callback();
                     }
                 }
             }
         }
+        function createColumn(col, timeout, callback) {
+            setTimeout(() => {
+                array.push({
+                    offsetY: 0,
+                    interval: setInterval(draw, 30, col),
+                    callback: callback
+                })
+            }, timeout)
+        }
 
         $(() => {
-            $("#spin").click(() => {
+            $("#spin").on("click", () => {
 
                 $("#spin").attr('disabled', "disabled");
 
-                for (var i = 0; i < array.length; i++) {
+                for (let i = 0; i < array.length; i++) {
                     array[i].offsetY = 0;
-
                     clearInterval(array[i].interval);
                 }
 
-                for (var i = 0; i < 5; i++) {
-
-                    var callback;
+                for (let i = 0; i < 5; i++) {
+                    let callback;
 
                     if (i == 4) {
-                        callback = function () {
+                        callback = () => {
                             $("#spin").removeAttr('disabled');
                         };
                     }
@@ -123,35 +122,25 @@
                     createColumn(i, data.speed * i, callback);
                 }
 
-                array = [];
+                array.length = 0;
+                img.length = 0;
 
-                img = [];
-
-                for (var i = 0; i < 90; i++) {
+                for (let i = 0; i < 90; i++) {
                     img.push(coord[Math.floor(Math.random() * coord.length)]);
                 }
-               
+
             });
         });
 
-        function createColumn(col, timeout, callback) {
-            setTimeout(function () {
-                array.push( {
-                    offsetY: 0,
-                    interval: setInterval(draw, 30, col),
-                    callback: callback
-                })
-            }, timeout)
-        }
     }
 
     private loadConfig(): any {
-        var data;
+        let data;
 
         $.ajax({
             url: 'config.json',
             async: false,
-        }).done(function (response) {
+        }).done((response) => {
             data = response;
         });
 
@@ -160,5 +149,5 @@
 }
 
 window.onload = () => {
-    var machine = new SlotMachine();
+    let machine = new SlotMachine();
 };
